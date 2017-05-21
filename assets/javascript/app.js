41b50f1e33d6e8f1bc6f7369281205b18bf89cd2
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-05-21 13:59:13 -0500

    dummy html,basic css, button on clicks

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+$( document ).ready(function() {
+
+var triviaQuestions = [{
+	question: "",
+	answers: "",
+	correctAnswer: "", 
+}];
+
+$("#start-button").on("click",function() {
+	$("#box1").hide();
+	$("#box2").css("display","block");
+});
+
+$("#end-button").on("click",function() {
+	$("#box1").hide();
+	$("#box2").hide();
+	$("#box3").css("display","block");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
